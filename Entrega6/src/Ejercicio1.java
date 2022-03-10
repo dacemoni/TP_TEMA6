4ad9a520951ea9729439e321d5dcf0301249a80a
gabrielPelegri <gabriel.pelegri.riis@gmail.com>
2022-03-10 19:12:34 +0100

    Update Ejercicio1.java

--- Entrega6/src/Ejercicio1.java
+++ Entrega6/src/Ejercicio1.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
 public class Ejercicio1 {
-    
+    public static void Lanzar(Scanner teclado){
+
+    }
+public int devolverMayor(int a,int b){
+    if(a>b){
+        return a;
+    }else{
+        return b;
+    }
+}
+
+
+
 }
