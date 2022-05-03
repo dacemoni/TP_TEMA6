ef1169c90c830eb3cb33c6b6afc4e830ebb9a69a
gabrielPelegri <gabriel.pelegri.riis@gmail.com>
2022-05-03 09:55:53 +0200

    Avance

--- Entrega6/src/Ejercicio4.java
+++ Entrega6/src/Ejercicio4.java
@@ -1,3 +1,47 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Ejercicio4 {
-    
+    static ArrayList<String> ovejas;
+
+    public static void Lanzar(Scanner teclado) {
+        ovejas = new ArrayList<>();
+        rellenarArray();
+        while(true){
+            mostrarArray();
+        }
+        
+
+    }
+
+    public static void rellenarArray() {
+        for (int i = 0; i < 7; i++) {
+            if (i < 3) {
+                ovejas.add(">");
+            } else if (i > 3) {
+                ovejas.add("<");
+            } else {
+                ovejas.add(3, " ");
+            }
+
+        }
+    }
+
+    public static void mostrarArray() {
+        for (int i = 0; i < ovejas.size(); i++) {
+
+            System.out.print(" | " + ovejas.get(i));
+
+        }
+        System.out.print(" |");
+    }
+
+    public static void movimiento(int a) {
+        if (ovejas.get(a).equals(">")) {
+            if (ovejas.get(a++).equals("")) {
+                ovejas.set(a++, ">");
+            }
+        }
+
+    }
 }
--- Entrega6/src/Main.java
+++ Entrega6/src/Main.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner teclado=new Scanner(System.in);
-        Selector.Elegir(teclado);
+        Ejercicio4.Lanzar(teclado);
+        
+        //Selector.Elegir(teclado);
     }
 }
