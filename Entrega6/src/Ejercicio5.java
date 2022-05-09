d9724030094f93443a5899328cb9c096fe7a64dd
vnavacar <vnavacar@myuax.com>
2022-05-09 12:19:57 +0200

    ejercicio5 empezado

--- Entrega6/src/Ejercicio5.java
+++ Entrega6/src/Ejercicio5.java
@@ -1,7 +1,22 @@
 import java.util.Scanner;
 
 public class Ejercicio5 {
+    static char[][] tabla;
+    static int columnasmax = 40;
+    static int filasmax = 10;
     public static void Lanzar(Scanner teclado){
         
+    }
+    private static void dibujarTabla(){
+        for (int i = 0; i < columnasmax; i++) {
+
+			for (int j = 0; j < filasmax; j++) {
+
+				System.out.printf(tabla[i][j] + "");
+
+			}
+			System.out.println();
+		}
+
     }
 }
